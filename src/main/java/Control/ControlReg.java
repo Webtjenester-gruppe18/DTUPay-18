7c474f4596fd04d84ce2e2787f3a727a003588e4
EGVinkel <emilvinkel@gmail.com>
2020-01-10 13:43:13 +0100

    Bank interface

--- src/main/java/Control/ControlReg.java
+++ src/main/java/Control/ControlReg.java
@@ -1,5 +1,7 @@
 package Control;
 
+import Bank.Bank;
+import Bank.IBank;
 import Database.IDatabase;
 import Database.InMemoryDatabase;
 import Exception.ExceptionContainer;
@@ -11,20 +13,30 @@ public class ControlReg {
     private static ExceptionContainer exceptionContainer;
     private static TokenService tokenService;
     private static ValidationService validationService;
+    private static IBank bank;
 
     public static IDatabase getDatabase() {
-        if (database == null) database = new InMemoryDatabase(); return database;
+        if (database == null) database = new InMemoryDatabase();
+        return database;
+    }
+
+    public static IBank getBank() {
+        if (bank == null) bank = new Bank();
+        return bank;
     }
 
     public static ExceptionContainer getExceptionContainer() {
-        if (exceptionContainer == null) exceptionContainer = new ExceptionContainer(); return exceptionContainer;
+        if (exceptionContainer == null) exceptionContainer = new ExceptionContainer();
+        return exceptionContainer;
     }
 
     public static TokenService getTokenService() {
-        if (tokenService == null) tokenService = new TokenService(); return tokenService;
+        if (tokenService == null) tokenService = new TokenService();
+        return tokenService;
     }
 
     public static ValidationService getValidationService() {
-        if (validationService == null) validationService = new ValidationService(); return validationService;
+        if (validationService == null) validationService = new ValidationService();
+        return validationService;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws BankServiceException_Exception {
         BankService bank = new BankServiceService().getBankServicePort();
-//        User user = new User();
-//        user.setCprNumber("1212121212");
-//        user.setFirstName("Jane");
-//        user.setLastName("Doe");
-//
-//        bank.createAccountWithBalance(user, BigDecimal.valueOf(10000));
+/*        bank.transferMoneyFromTo(bank.getAccountByCprNumber("1212121213").getId(),
+                bank.getAccountByCprNumber("1212121212").getId(), BigDecimal.valueOf(500),
+                "Tak for i går");*/
 
+      /*  User user = new User();
+        user.setCprNumber("1212121213");
+        user.setFirstName("John");
+        user.setLastName("Doe");*/
 
-     /*   for (AccountInfo a : bank.getAccounts()) {
+      //  bank.createAccountWithBalance(user, BigDecimal.valueOf(10000));
+
+ /*       for (AccountInfo a : bank.getAccounts()) {
+            System.out.println(a.getAccountId());
             System.out.println(a.getUser().getFirstName() + " Balance "+ bank.getAccount(a.getAccountId()).getBalance());
+
+          for (Transaction t: bank.getAccount(a.getAccountId()).getTransactions())
+            System.out.println( "Tid "+ t.getTime()  + " Debtor "+  t.getDebtor() + " Description" + t.getDescription());
 
         }*/
     }
