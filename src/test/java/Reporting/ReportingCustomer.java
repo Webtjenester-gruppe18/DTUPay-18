6513d212d723c2c54b3a83c5540de815931d420f
AM <lgv2800@gmail.com>
2020-01-13 19:37:39 +0100

    Added base file for ReportingCustomer with generated stepdefinitions.

---
+++ src/test/java/Reporting/ReportingCustomer.java
@@ -0,0 +1,71 @@
+package Reporting;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ReportingCustomer {
+
+    // -- Common
+
+    @Given("the customer is registered")
+    public void the_customer_is_registered() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the customer views his transactions")
+    public void the_customer_views_his_transactions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the customer is shown his transactions")
+    public void the_customer_is_shown_his_transactions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    // -- Merchant
+
+    @When("the customer filters his transactions by merchant")
+    public void the_customer_filters_his_transactions_by_merchant() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the customer is shown his transactions only for the specified merchant")
+    public void the_customer_is_shown_his_transactions_only_for_the_specified_merchant() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    // -- Date
+
+    @When("the customer filters his transactions by date")
+    public void the_customer_filters_his_transactions_by_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the customer is shown his transactions only for the specified date")
+    public void the_customer_is_shown_his_transactions_only_for_the_specified_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    // -- Amount
+
+    @When("the customer filters his transactions by transaction amount")
+    public void the_customer_filters_his_transactions_by_transaction_amount() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the customer is shown his transactions only for the specified transaction amount")
+    public void the_customer_is_shown_his_transactions_only_for_the_specified_transaction_amount() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
